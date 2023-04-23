d73804b69440d02710c5fa66d048f8292e6fe9b1
Lucas Darie <lucas.darie@etu.umontpellier.fr>
2023-04-23 02:07:42 +0200

    Finished inventory refactor.

--- Assets/!/Code/Scripts/Player.cs
+++ Assets/!/Code/Scripts/Player.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public MouseItem mouseItem = new MouseItem();
     public PlayerMouse playerMouse = new PlayerMouse();
     
     void Start() {
