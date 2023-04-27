30404a52f33bbeff7664dd9a620b7623824123ce
DisaroSkell <Hydralisk34980@gmail.com>
2023-04-27 09:50:13 +0200

    MazePath constructor, missing throws

--- Assets/!/Code/Scripts/PeriodicTable/MazePath.cs
+++ Assets/!/Code/Scripts/PeriodicTable/MazePath.cs
@@ -6,86 +6,125 @@ public class MazePath {
 
     private List<Direction> directions;
 
+    // TODO : Refactor this
     public MazePath(Maze maze, (int, int)[] indexSet, Direction[] directions) {
         if (indexSet.Length != indexSet.Distinct().Count()) {
-            // STOP HERE
+            // STOP HERE: Can't create path from duplicates cells
         }
 
         if (indexSet.Length != directions.Length) {
-            // STOP HERE
+            // STOP HERE: Must have a direction (even if it's Direction.None) for every cell
         }
 
         this.indexSet = new List<(int, int)>();
         this.directions = new List<Direction>();
 
+        // We create a way between every cell
         for (int i = 0; i < indexSet.Length-1; i++) {
             int line = indexSet[i].Item1;
             int column = indexSet[i].Item2;
             Direction direction = directions[i];
 
+            // We first go in the direction given and add all cells in the way until aligned
             switch (direction) {
                 case Direction.North :
-                    if (line > 0 && line >= indexSet[i+1].Item1){
+                    // Go North
+                    if (line >= indexSet[i+1].Item1){
                         while (line != indexSet[i+1].Item1) {
                             this.indexSet.Add((line, column));
                             this.directions.Add(direction);
                             line--;
                         }
                     } else {
-                        // STOP HERE
+                        // STOP HERE: We are going opposite direction => can't create path
                     }
                     break;
                 case Direction.South :
-                    if (line < maze.GetHSize() - 1 && line <= indexSet[i+1].Item1){
+                    // Go South
+                    if (line <= indexSet[i+1].Item1){
                         while (line != indexSet[i+1].Item1) {
                             this.indexSet.Add((line, column));
                             this.directions.Add(direction);
                             line++;
                         }
                     } else {
-                        // STOP HERE
+                        // STOP HERE: We are going opposite direction => can't create path
                     }
                     break;
                 case Direction.East :
-                    if (column < maze.GetVSize() - 1 && column <= indexSet[i+1].Item2){
+                    // Go East
+                    if (column <= indexSet[i+1].Item2){
                         while (column != indexSet[i+1].Item2) {
                             this.indexSet.Add((line, column));
                             this.directions.Add(direction);
                             column++;
                         }
                     } else {
-                        // STOP HERE
+                        // STOP HERE: We are going opposite direction => can't create path
                     }
                     break;
                 case Direction.West :
-                    if (column > 0 && column >= indexSet[i+1].Item2){
+                    // Go West
+                    if (column >= indexSet[i+1].Item2){
                         while (column != indexSet[i+1].Item2) {
                             this.indexSet.Add((line, column));
                             this.directions.Add(direction);
                             column--;
                         }
                     } else {
-                        // STOP HERE
+                        // STOP HERE: We are going opposite direction => can't create path
                     }
                     break;
             }
-            // TODO Go on next cell if aligned
 
             // First detect what direction to take
-            // Then move to the cell
+            // Then move to the cell and take all cells on the way
 
             if (line == indexSet[i+1].Item1) {
-                // Aligned horizontally
+                if (column > indexSet[i+1].Item2) {
+                    // Go West
+                    direction = Direction.West;
+                    while (column != indexSet[i+1].Item2) {
+                        this.indexSet.Add((line, column));
+                        this.directions.Add(direction);
+                        column--;
+                    }
+                } else {
+                    // Go East
+                    direction = Direction.East;
+                    while (column != indexSet[i+1].Item2) {
+                        this.indexSet.Add((line, column));
+                        this.directions.Add(direction);
+                        column++;
+                    }
+                }
             } else if (column == indexSet[i+1].Item2) {
-                // Aligned vertically
+                if (line > indexSet [i+1].Item1) {
+                    // Go North
+                    direction = Direction.North;
+                    while (line != indexSet[i+1].Item1) {
+                        this.indexSet.Add((line, column));
+                        this.directions.Add(direction);
+                        line--;
+                    }
+                } else {
+                    // Go South
+                    direction = Direction.South;
+                    while (line != indexSet[i+1].Item1) {
+                        this.indexSet.Add((line, column));
+                        this.directions.Add(direction);
+                        line++;
+                    }
+                }
             } else {
-                // STOP HERE
+                // STOP HERE: Not aligned with next cell => Missing direction
             }
         }
-        // TODO verify that we add all the cells on the way
+        // We add last cell
+        this.indexSet.Add(indexSet[indexSet.Length-1]);
 
         if (this.indexSet.Count != this.indexSet.Distinct().Count()) {
-            // STOP HERE
+            // STOP HERE: We added the same cell twice => not a valid way
         }
     }
 
