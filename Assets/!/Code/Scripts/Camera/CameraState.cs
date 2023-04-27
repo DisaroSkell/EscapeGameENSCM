debb8ff7ca840639c5906ea81d6e023d95835486
DisaroSkell <Hydralisk34980@gmail.com>
2023-04-27 16:26:57 +0200

    Room 2 camera animations

--- Assets/!/Code/Scripts/Camera/CameraState.cs
+++ Assets/!/Code/Scripts/Camera/CameraState.cs
@@ -1,11 +1,16 @@
 /* This enum is used to represent the current state of the camera. */
 public enum CameraState {
-    Unfocused, // Focus = 0
-    FocusingCabinet, // Focus = 1
-    TableGroup1, // Focus = 2
-    TableGroup2, // Focus = 3
-    FocusingDesk1, // Focus = 4
-    FocusingDesk2, // Focus = 5
-    FocusingPeriodicTable, // Focus = 6
-    UnfocusedRoom2 // Focus = 7
+    Unfocused,              // Focus = 0
+    FocusingCabinet,        // Focus = 1
+    FocusingTableGroup1,    // Focus = 2
+    FocusingTableGroup2,    // Focus = 3
+    FocusingDesk1,          // Focus = 4
+    FocusingDesk2,          // Focus = 5
+    FocusingPeriodicTable,  // Focus = 6
+    UnfocusedRoom2,         // Focus = 7
+    FocusingBookPile1,      // Focus = 8
+    FocusingGloves,         // Focus = 9
+    FocusingBookPile3,      // Focus = 10
+    FocusingSafe,           // Focus = 11
+    FocusingSulfate         // Focus = 12
 }
