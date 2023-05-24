0c1f2b7c2c29622a7dd549d3f3f4a33838ea3e2c
Lucas Darie <lucas.darie@etu.umontpellier.fr>
2023-05-24 10:01:29 +0200

    repair bugs with UVLamp

--- Assets/!/Code/Scripts/Inventories/FlippedItem/InventoryFlipper.cs
+++ Assets/!/Code/Scripts/Inventories/FlippedItem/InventoryFlipper.cs
@@ -10,28 +10,49 @@ public class InventoryFlipper : MonoBehaviour
 
     public TextMeshProUGUI? button_text;
 
+    public AbstractInventoryDisplay? display;
+
     public bool power;
+
+    private bool hasChanged = true;
 
     private void Start() {
         if(button_text is null) return;
-        if(power) button_text.text = "ON";
-        else button_text.text = "OFF";
+        if(power) button_text.text = "OFF";
+        else button_text.text = "ON";
+    }
+
+    private void Update() {
+        SetAllCardState();
+        if(display is not null) {
+            if(hasChanged) {
+                display.UpdateDisplay();
+                hasChanged = false;
+            }
+        }
+        
     }
 
     public void FlipAllItem() {
-        if(inv is null) return;
         if(button_text is null) return;
+        power = !power;
+        if(power) button_text.text = "OFF";
+        else button_text.text = "ON";
+    }
+
+    public void SetAllCardState() {
+        if(inv is null) return;
         for (int i = 0; i < inv.Length; i++)
         {
             ItemObject? item = inv.GetItem(i);
             if(item is null) continue;
             if(item.GetType() == typeof(MatchesCardObject)) {
                 MatchesCardObject it = (MatchesCardObject)item;
-                it.Flip();
+                if(it.fliped != power) {
+                    it.fliped = power;
+                    hasChanged = true;
+                }
             }
         }
-        power = !power;
-        if(power) button_text.text = "ON";
-        else button_text.text = "OFF";
     }
 }
